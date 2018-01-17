861afbc9996d5735c22a7dae9e6ff4ae9788b554
“jianglei-00869712” <jianglei2@yto.net.cn>
2018-01-17 11:38:06 +0800

    添加tableView

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>{
+    
+    UITableView *_tableView;
+}
 
 @end
 
@@ -17,8 +20,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _tableView =[[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStyleGrouped];
+    _tableView.dataSource =self;
+    _tableView.delegate =self;
+    _tableView.estimatedRowHeight =0;
+    _tableView.estimatedSectionFooterHeight =0;
+    _tableView.estimatedSectionHeaderHeight =0;
+    [self.view addSubview:_tableView];
+    
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 44.0f;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    
+    return 0.01f;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    
+    return 0.01f;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *identifierCell =@"Cell";
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:identifierCell];
+    if (!cell) {
+        cell =[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifierCell];
+    }
+    
+    return cell;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
